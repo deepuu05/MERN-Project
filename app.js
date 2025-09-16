03c4db40b0ae38092d1e984312e791e74927b4be
deepanshu <deepanshu201204@gmail.com>
2025-09-17 01:22:56 +0530

    new commit

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const postModel = require('./models/post');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const bcrypt = require('bcrypt') ;
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const user = require('./models/user');
 
 app.set('view engine','ejs');
 app.use(express.json());
@@ -49,10 +50,61 @@ app.post('/register', async (req,res)=>{
 app.get('/login',(req,res)=>{
     res.render('login')
 })
-app.post('/login',(req,res)=>{
-    res.render('profile')
+app.post('/login',async (req,res)=>{
+    let{email,password}= req.body;
+
+    let user = await userModel.findOne({email:email});
+    // agar user nhi milta
+    // if(!user) return res.status(500).send("Something went wrong")
+    if(!user) return res.status(500).redirect("/login")
+    
+    // agar user milta hai
+    bcrypt.compare(password,user.password,function(err,result){
+        if(result) {
+            let token =jwt.sign({email:email,userid:user._id},"shius");
+            res.cookie("token",token)
+            // res.status(200).send("you can login")
+            res.status(200).redirect("/profile")
+        }
+        else res.redirect("/login")
+        
+    })
 })
 
+app.get('/profile', isLoggedIn , async (req,res)=>{
+    console.log(req.user)
+   let userdata= await userModel.findOne({email:req.user.email}).populate("posts");
+   console.log(userdata)
+    res.render('profile',{userdata});
+})
+
+app.get('/logout',(req,res)=>{
+    res.cookie('token',"");
+    res.redirect("/login")
+})
+
+app.post("/post",isLoggedIn, async (req,res)=>{
+    let user = await userModel.findOne({email: req.user.email});
+    let {content} = req.body;
+    let post = await postModel.create({
+        user: user._id,
+        content
+    })
+ user.posts.push(post._id)
+ await user.save();
+ res.redirect('/profile')
+})
+
+
+function isLoggedIn(req,res,next){
+    // if(req.cookies.token==="") res.send("you must be logged in");
+    if(req.cookies.token==="") res.redirect("/login");
+    else{
+       let data= jwt.verify(req.cookies.token,"shius")
+       req.user= data;// data ko access krne ke liye kisi aur route me ki kon banda hai ye
+    }
+    next();
+}
 app.listen(3000,()=>{
     console.log("its running");
 });
